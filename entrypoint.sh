9b44049092ec502eb65f017a6f9f82dcf60f9d0a
Sergey Aksenov <sergey.aksenov@constructor.tech>
2023-06-04 23:02:35 +0200

    feat: use 4 Gunicorn workers instead of 1

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-gunicorn --chdir /django -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8001 config.asgi:application &
-gunicorn --chdir /fastapi -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8002 main:app
+gunicorn -w 4 --chdir /django -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8001 config.asgi:application &
+gunicorn -w 4 --chdir /fastapi -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8002 main:app
