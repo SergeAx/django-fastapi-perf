1a8efd12318d6d85c7be18e9f08dbf399ade45df
Sergey Aksenov <sergey.aksenov@constructor.tech>
2023-06-04 23:01:09 +0200

    fix: Durn off DEBUG for Django

--- django/config/settings.py
+++ django/config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jiqtrnk09jqh$*5h4m0j1u$f^#np)p^$+8-bc99rg4+(r3!z8p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
